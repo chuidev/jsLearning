1537a9b4efb3e02d47b7c982cad2c23161240e11
chuidev <churkin1989@yandex.ru>
2022-05-30 15:01:19 +0300

    main

--- js_1.js
+++ js_1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?'),
+let money = +prompt('Ваш бюджет на месяц?'),
     time = prompt('Введите дату в формате YYYY-MM-DD'),
     appData = {
       budget: money,
@@ -11,20 +11,32 @@ let money = prompt('Ваш бюджет на месяц?'),
       savings: false
     };
 
-for (let i=1; i<=2; i++) {
+for (let i = 0; i < 2; i++) {
   let a = prompt('Введите обязательную статью расходов в этом месяце'),
       b = +prompt('Во сколько обойдется?');
 
-  if (typeof(a) === 'string' && typeof(a) != null && a != ' ' &&
-      typeof(b) === 'number' && typeof(b) != null && b != ' ') {
-  
-        appData.expenses[a] = b;
-      } else {
-        console.log('Введены неправильные данные');
-      }
+  if ( typeof(a) === 'string' && typeof(a) != null
+        && typeof(b) === 'number' && typeof(b) != null
+        && a != '' && b != '' && a.length < 50) {
+    console.log('done');
+    appData.expenses[a] = b;  
+  } else {
+    console.log('Введены неправильные данные');
+  }
 }
 
-console.log('Массив данных: ');
-console.log(appData);
+appData.moneyPerDay = appData.budget / 30;
 
-console.log('Бюджет на 1 день: ' + parseInt(appData.budget/30));
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+  console.log('Высокий уровень достатка');
+} else {
+  console.log('Произошла ошибка');
+}
+
+console.log(appData);
--- js_2.js
+++ js_2.js
@@ -1,2 +1,70 @@
 'use strict';
 
+// let num = 50;
+
+// --------------------- условия ---------------------
+
+// if (num < 49) {
+//   console.log('Неверно');
+// } else if (num > 100) {
+//   console.log('Много');
+// } else {
+//   console.log('Верно');
+// }
+
+// (num == 50) ? console.log('Верно') : console.log('Неверно'); // тернарный оператор
+
+// switch (num) {
+//   case num < 49 : 
+//     console.log('Неверно');
+//     break;
+//   case num > 100 :
+//     console.log('Много');
+//     break;
+//   case num >= 80 :
+//     console.log('Все еще много');
+//     break;
+//     // case num == 50 : - Неверная запись
+//   case 50 :
+//     console.log('Верно');
+//     break;
+//   default :
+//     console.log('что-то пошло не так');
+//     break;
+// }
+
+// --------------------- циклы ---------------------
+
+// Цикл для того, чтобы проверить, а потом сделать
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// Цикл для того, чтобы сделать, а потом проверить
+// do {
+//   console.log(num);
+//   num++;
+// } while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//   if (i == 6) {
+//     // break;
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let week = ['Monday', 'Thuesday', 'Wensday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+// for (let i = 0; i < week.length; i++) {
+//   console.log(week[i]);
+// }
+
+let arr = ['21344', '12314', '32543', '72342', '42123', '79023'];
+
+for (let i = 0; i < arr.length; i++) {
+  if ((arr[i].substr(0,1) == 7) || (arr[i].substr(0,1) == 3)) {
+    console.log(arr[i]);
+  }
+}
