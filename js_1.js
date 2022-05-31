ccf83bda95f66f6871b3e55b012095d6bd5f874a
chuidev <churkin1989@yandex.ru>
2022-05-31 16:02:34 +0300

    main

--- js_1.js
+++ js_1.js
@@ -1,42 +1,87 @@
 'use strict';
 
-let money = +prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD'),
-    appData = {
+let money, time;
+
+function start() {
+  money = +prompt('Ваш бюджет на месяц?');
+  time = prompt('Введите дату в формате YYYY-MM-DD');
+
+  while( isNaN(money) || money == '' || money == null ) {
+    money = +prompt('Ваш бюджет на месяц?');
+  }
+}
+start();
+
+let  appData = {
       budget: money,
       timeData: time,
       expenses: {},
       optionalExpenses: {},
       income: [],
-      savings: false
+      savings: true
     };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt('Введите обязательную статью расходов в этом месяце'),
-      b = +prompt('Во сколько обойдется?');
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце'),
+        b = +prompt('Во сколько обойдется?');
+  
+    if ( typeof(a) === 'string' && typeof(a) != null
+          && typeof(b) === 'number' && typeof(b) != null
+          && a != '' && b != '' && a.length < 50) {
+      console.log('done');
+      appData.expenses[a] = b;  
+    } else {
+      i = i - 1;
+    }
+  }  
+}
+chooseExpenses();
 
-  if ( typeof(a) === 'string' && typeof(a) != null
-        && typeof(b) === 'number' && typeof(b) != null
-        && a != '' && b != '' && a.length < 50) {
-    console.log('done');
-    appData.expenses[a] = b;  
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+  return appData.moneyPerDay;
+}
+detectDayBudget();
+
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+  } else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
   } else {
-    console.log('Введены неправильные данные');
+    console.log('Произошла ошибка');
+  }  
+}
+detectLevel();
+
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt('Какова сумма накоплений?'),
+        perc = +prompt('Под какой процент?');
+
+    appData.monthIncome = save / 100 / 12 * perc;
+    alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
   }
 }
+checkSavings();
 
-appData.moneyPerDay = appData.budget / 30;
-
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-  console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log('Средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-  console.log('Высокий уровень достатка');
-} else {
-  console.log('Произошла ошибка');
+function chooseOptExpenses() {
+  for (let i = 1; i < 4; i++) {
+    let a = prompt('Введите статью необязательных расходов в этом месяце'),
+        b = +prompt('Во сколько обойдется?');
+  
+    if ( typeof(a) === 'string' && typeof(a) != null
+          && typeof(b) === 'number' && typeof(b) != null
+          && a != '' && b != '' && a.length < 50) {
+      console.log('done');
+      appData.optionalExpenses[i] = b;  
+    } else {
+      i = i - 1;
+    }
+  }  
 }
-
-console.log(appData);
+chooseOptExpenses();
---
+++ js_3.js
@@ -0,0 +1,128 @@
+'use strict';
+
+// ------------------------------ Функции ------------------------------
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//   alert(text);
+//   let num = 10;
+//   console.log(num);
+// }
+
+// showFirstMessage('Привет');
+
+// console.log(num);
+
+// var calc = function(a, b) {
+//   return (a + b);
+// }
+
+// let calc = (a, b) => a + b;
+
+// console.log(calc(3, 4));
+// console.log(calc(8, 4));
+
+// function retVar() {
+//   let num = 50;
+//   return num;
+// }
+
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+// let str = 'test';
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let twelve = '12.2px';
+
+// // console.log(Math.round(twelve));
+// console.log(parseInt(twelve));
+// console.log(parseFloat(twelve));
+
+// -------------------------- Дополнительное задание --------------------------
+
+// есть str = “урок-3-был слишком легким”
+// Сделать так, чтобы строка начиналась с большой буквы
+
+// let str = 'урок-3-был слишком легким',
+//     first = str.charAt(0),
+//     firstChanged = first.toUpperCase(),
+//     word = /-/gi,
+//     newstr_3 = '';
+
+// // console.log('Начальная строка: ' + str);
+
+//   for (let i = 1; i < str.length; i++) {
+//     firstChanged = firstChanged + str[i];
+//     var newstr = firstChanged.replace(word, ' ');
+//   }
+
+// console.log('Замена первой буквы на ЗАГЛАВНУЮ:');
+// console.log(firstChanged);
+// console.log('');
+// console.log('Замена "-"" на " "');
+// console.log(newstr);
+
+// let newWord = / легким/gi,
+//     newstr_2 = newstr.replace(newWord, '');
+
+// console.log('');
+// console.log('Убрали слово "легким"');
+// console.log(newstr_2);
+
+// let dlina = newstr_2.length - 2;
+
+// for (let i = 0; i < dlina; i++) {
+//   newstr_3 = newstr_3 + newstr_2[i];
+// }
+
+// console.log('');
+// console.log('Замена последних двух букв на "о"')
+// console.log(newstr_3 + 'оо');
+
+// // Вывести в консоль квадратный корень из суммы кубов его элементов (Да, человека нужно исключить)
+// let arr = [20, 33, 1, 'Человек', 2, 3],
+//     arr2 = [],
+//     sum = 0;
+    
+// for (let i = 0; i < arr.length; i++) {
+//   if ( typeof(arr[i]) === 'string' ) {
+//     arr.splice(i, 1);
+//   }
+//   sum = sum + (arr[i] ** 3);
+//   arr2.push(arr[i] ** 3);
+// }
+
+// console.log('');
+// console.log('Начальный массив:');
+// console.log(arr);
+
+// console.log('');
+// console.log('Массив с элементами в кубе:');
+// console.log(arr2);
+// console.log('Сумма элементов в кубе: ' + sum);
+// console.log('Квадратный корень из суммы элементов в кубе: ' + Math.sqrt(sum));
+
+function func(text) {
+  let dlina = text.length - 2;
+
+  if ( typeof(text) != 'string' && text[0] == ' ' && text[text.length - 1] == ' ') {
+    alert('Функции передана не строка');
+  } else {
+    console.log('Передана строка:');
+    console.log(text);
+    alert(text.substr(1, dlina));
+  }
+
+  if ( text.length > 5) {
+    console.log(text.slice(0, 5) + '...');
+  } else {
+    console.log('длина строки меньше 50 символов');
+  }
+};
+
+func(' 123 123 ');
